bdb706e8c96e9b17e731a1e50d01d71247773d4c
Joshua Lucas <joshuamlucas91@gmail.com>
2020-07-27 18:33:31 -0400

    made inputs responsive to thier contianer

--- src/FormComponents/Input.js
+++ src/FormComponents/Input.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import screenSize from '../Utills/ContinerSize'
 
 const Label = styled.label`
   font-size: 1.5rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  color: ${(props) => props.theme.primaryTextColor};
 `
 const InputElement = styled.input`
-  font-size: 1.5rem;
-  margin-top: 1rem;
+  font-size: 1rem;
+  margin-top: 0.25rem;
+  margin-bottom: 2rem;
   padding: 1rem;
+
+  @media ${screenSize.md} {
+    padding: 0.5rem;
+  }
 `
 
 const Input = ({ label, type, value, event }) => {
