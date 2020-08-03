1cc3f849df1a7256e9d6c7737cb54d4b6c3c6138
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-03 09:19:57 -0400

    add stroybook add ons

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,14 @@ module.exports = {
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
-    '@storybook/addon-links'
+    '@storybook/addon-links',
+    '@storybook/addon-contexts/register',
+    '@storybook/addon-backgrounds/register',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true
+      }
+    }
   ]
 }
