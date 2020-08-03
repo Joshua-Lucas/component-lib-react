5889ed69de5dddabee46c118425b4365e48c0355
Joshua Lucas <joshuamlucas91@gmail.com>
2020-08-03 09:36:35 -0400

    updated to add new buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import Input from './FormComponents/Input'
-import Buttons from './FormComponents/Buttons'
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton
+} from './FormComponents/Buttons'
 import FormDiv from './ContainerComponents/FormDiv'
 import TextArea from './FormComponents/TextArea'
 import Dropdown from './FormComponents/Dropdown'
 import UtilityStyles from './Utills/index'
 
-export { Input, Buttons, TextArea, FormDiv, Dropdown, UtilityStyles }
+export {
+  Input,
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+  TextArea,
+  FormDiv,
+  Dropdown,
+  UtilityStyles
+}
